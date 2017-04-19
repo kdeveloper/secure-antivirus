244a5724ea7c1ae1a87d08212e4588f48ab0d660
devangvadalia <devangvadalia@devangs-macbook-pro.local>
2017-04-19 23:48:33 +0530

    Initial Commit

---
+++ Secure Antivirus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Secure Antivirus
+//
+//  Created by Devang vadalia on 19/04/17.
+//  Copyright © 2017 Devang vadalia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
