0329b0ca7c053dccd11d3a25f3fe3a04827b9fc6
devangvadalia <devangvadalia@devangs-macbook-pro.local>
2017-04-23 23:14:52 +0530

    1. Button added

---
+++ DashBoardViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DashBoardViewController.swift
+//  Secure Antivirus
+//
+//  Created by Devang vadalia on 23/04/17.
+//  Copyright © 2017 Devang vadalia. All rights reserved.
+//
+
+import Cocoa
+
+class DashBoardViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.layer?.backgroundColor = NSColor.red.cgColor
+        // Do view setup here.
+    }
+    
+}
--- TopMenuBarViewController.swift
+++ TopMenuBarViewController.swift
@@ -8,8 +8,59 @@
 
 import Foundation
 import Cocoa
+let storyBoard = NSStoryboard(name: "StoryBoard", bundle: nil) as NSStoryboard
+var objDashBoard = DashBoardViewController()
+var objBottomoView = BottomViewController()
 
 class TopMenuBarViewController :NSViewController
 {
+    @IBOutlet var backView: NSView!
+    
+    @IBOutlet var btnDashBoard: NSButton!
+    @IBOutlet var btnScan: NSButton!
+    @IBOutlet var btnReport: NSButton!
+    @IBOutlet var btnShield: NSButton!
+    
+    override func viewDidLoad() {
+        self.backView.wantsLayer = true
+        self.backView.layer?.backgroundColor = NSColor.black.cgColor
+    }
+    
+    
+    @IBAction func dashBoardClicked(_ sender: Any) {
+        
+        btnDashBoard.image = NSImage.init(named: "Home_Orange")
+        btnScan.image = NSImage.init(named: "Scan_White")
+        btnReport.image = NSImage.init(named: "Report_White")
+        btnShield.image = NSImage.init(named: "Shield_White")
+        
+        objDashBoard = storyBoard.instantiateController(withIdentifier: "DashBoardVC") as! DashBoardViewController
+        self.presentViewControllerAsModalWindow(objDashBoard)
+        
+    }
+    
+    @IBAction func scanClicked(_ sender: Any) {
+        
+        btnDashBoard.image = NSImage.init(named: "Home_White")
+        btnScan.image = NSImage.init(named: "Scan_Orange")
+        btnReport.image = NSImage.init(named: "Report_White")
+        btnShield.image = NSImage.init(named: "Shield_White")
+    }
+    
+    @IBAction func reportsClicked(_ sender: Any) {
+        
+        btnDashBoard.image = NSImage.init(named: "Home_White")
+        btnScan.image = NSImage.init(named: "Scan_White")
+        btnReport.image = NSImage.init(named: "Report_Orange")
+        btnShield.image = NSImage.init(named: "Shield_White")
+    }
+    
+    @IBAction func shieldClicked(_ sender: Any) {
+        
+        btnDashBoard.image = NSImage.init(named: "Home_White")
+        btnScan.image = NSImage.init(named: "Scan_White")
+        btnReport.image = NSImage.init(named: "Report_White")
+        btnShield.image = NSImage.init(named: "Shield_Orange")
+    }
     
 }
